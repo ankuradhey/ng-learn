647aa6017b59e19df6e2fddae64cb69624a4ef13
Ankit Sharma <anksharm19@publicisgroupe.net>
2020-04-09 10:15:34 +0530

    Pipe logic update

--- src/day6/src/app/pipes/sortby.pipe.ts
+++ src/day6/src/app/pipes/sortby.pipe.ts
@@ -4,8 +4,8 @@ import { orderBy } from 'lodash';
 @Pipe({ name: 'sortBy' })
 export class SortByPipe implements PipeTransform {
 
-  transform(value: any[], order = '', column: string = ''): any[] {
-    if((!value || !order)) {
+  transform(value: any[], order = 'asc', column: string = ''): any[] {
+    if(!value) {
             return value; 
     }
     return orderBy(value, [column], [order]);
